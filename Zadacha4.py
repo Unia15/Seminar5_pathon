64d019b0016401231a58472d7cf869200c2c019b
Julia.Ef <jullia.ef@yandex.ru>
2022-12-11 19:51:13 +0300

    Zadacha4.py

---
+++ Zadacha4.py
@@ -0,0 +1,36 @@
+#Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+with open('text1.txt', 'w', encoding='UTF-8') as file:
+    file.write(input('Напишите текст для сжатия: '))
+with open('text1.txt', 'r') as file:
+    my_text = file.readline()
+    text_compression = my_text.split()
+
+print(my_text)
+
+
+def rle_encode(text):
+    enconding = ''
+    prev_char = ''
+    count = 1
+    if not text:
+        return ''
+
+    for char in text:
+        if char != prev_char:
+            if prev_char:
+                enconding += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    else:
+        enconding += str(count) + prev_char
+        return enconding
+
+
+text_compression = rle_encode(my_text)
+
+with open('text2.txt', 'w', encoding='UTF-8') as file:
+    file.write(f'{text_compression}')
+print(text_compression)
